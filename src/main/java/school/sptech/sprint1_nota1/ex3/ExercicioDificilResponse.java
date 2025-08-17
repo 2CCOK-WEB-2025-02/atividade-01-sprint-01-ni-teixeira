1c097208f69a4b4ec3f4be7dbaa669075307d580
ni-teixeira <nicoly.teixeira04@gmail.com>
2025-08-17 14:15:14 -0300

    v2

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
@@ -2,13 +2,13 @@ package school.sptech.sprint1_nota1.ex3;
 
 public class ExercicioDificilResponse {
 
-    private Integer enesimoTermo;
-    private Integer soma;
+    private int enesimoTermo;
+    private int soma;
 
     public ExercicioDificilResponse() {
     }
 
-    public ExercicioDificilResponse(Integer enesimoTermo, Integer soma) {
+    public ExercicioDificilResponse(int enesimoTermo, int soma) {
         this.enesimoTermo = enesimoTermo;
         this.soma = soma;
     }
@@ -18,16 +18,15 @@ public class ExercicioDificilResponse {
         if (num > 0) {
             int a = 0;
             int b = 1;
+            int temp = a + b;
+            int c = 0;
 
             if (num == 1) {
                 enesimoTermo = 1;
                 soma = 1;
             }
 
-            int temp = a + b;
-            int c = 0;
-
-            for (int i = 3; i <= num; i++) {
+            for (int i = 2; i <= num; i++) {
                 c = a + b;
                 temp += c;
                 a = b;
@@ -40,12 +39,20 @@ public class ExercicioDificilResponse {
 
     }
 
-    public Integer getEnesimoTermo() {
+    public int getEnesimoTermo() {
         return enesimoTermo;
     }
 
-    public Integer getSoma() {
+    public void setEnesimoTermo(int enesimoTermo) {
+        this.enesimoTermo = enesimoTermo;
+    }
+
+    public int getSoma() {
         return soma;
+    }
+
+    public void setSoma(int soma) {
+        this.soma = soma;
     }
 
 }
